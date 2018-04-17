7d8c0501dc387d3283ea041bc51788f992c3e16f
Denis <denis.tolmachev@gmail.com>
2018-04-17 21:33:49 +0300

    create header & footer

--- wp-content/themes/wine-store-wp/footer.php
+++ wp-content/themes/wine-store-wp/footer.php
@@ -1,5 +1,40 @@
+
+    <footer>
+        <div class="footer-text">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eu mi nec justo vestibulum auctor. Phasellus sollicitudin diam at feugiat dictum. Cras facilisis lorem metus, et tincidunt magna imperdiet porttitor. Sed magna velit, hendrerit non aliquet quis, posuere dapibus est.</p>
+        </div>
+        <div class="contacts">
+            <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div class="phone-st">
+                        <img src="./img/phone-st.png" alt="">
+                        <p>Тел. (061) 271-94-53</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
+                    <div class="phone-mob">
+                        <img src="./img/phone-mob.png" alt="">
+                        <p>Моб. 067-95-92-463</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12 col-sm-12 col-xs-12">
+                    <div class="e-mail">
+                        <img class="mail" src="./img/mail.png" alt="">
+                        <a href="mailto:Zagorulko_v_v@mail.ru">e-mail: Zagorulko_v_v@mail.ru</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar-footer">
+            <? wp_nav_menu(array('menu' => 'nav-menu', 'menu_class' => 'nav-menu')); ?>
+        </nav>
+        <div class="copyright">
+            <p>Все права защищены 2016</p>
+        </div>
+    </footer>
+
 </div>
-<footer></footer>
-<?php wp_footer(); ?>
+
 </body>
 </html>
--- wp-content/themes/wine-store-wp/header.php
+++ wp-content/themes/wine-store-wp/header.php
@@ -8,4 +8,30 @@
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
-<div class="wrapper">
+<div class="container">
+    <header>
+        <div class="nav-field">
+            <div class="row">
+                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-6">
+                    <nav class="navbar-header">
+                        <? wp_nav_menu(array('menu' => 'nav-menu', 'menu_class' => 'nav-menu')); ?>
+                    </nav>
+                    <div id="sliding-menu"></div>
+                </div>
+                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-6">
+                    <div class="register-box">
+                        <div class="cart" onclick="location.href = ''">
+
+                        </div>
+                        <div class="buttons" id="reg-buttons">
+                            <button class="cd-signin" onclick="location.href = ''">вход</button>
+                            <button class="cd-signup" onclick="location.href = ''">регистрация</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-circle">
+            <p>авторские селекционные формы и сорта винограда</p>
+        </div>
+    </header>
